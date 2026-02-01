e369b150762407ea437fdff560dfdf3181e6019c
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-01 23:07:55 +0100

    feat(welcome): add background image

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,12 +1,19 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { View } from "react-native";
+import { Image, View } from "react-native";
 import { RootStackParamList } from "../navigation/RootStack";
 
 type WelcomeScreenProps = NativeStackScreenProps<RootStackParamList, "Welcome">;
 
 const WelcomeScreen = ({ navigation }: WelcomeScreenProps) => {
-  return <View className="flex flex-1"></View>;
+  return (
+    <View className="flex flex-1 justify-end">
+      <Image
+        source={require("../../assets/images/welcome.webp")}
+        className="w-full h-full absolute"
+      />
+    </View>
+  );
 };
 
 export default WelcomeScreen;
