914e4614c27ad16f4d0c3d4b807fc013b69642ca
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-01 23:17:46 +0100

    feat(welcome): add LinearGradient

--- src/screens/WelcomeScreen.tsx
+++ src/screens/WelcomeScreen.tsx
@@ -1,4 +1,5 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { LinearGradient } from "expo-linear-gradient";
 import React from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import { RootStackParamList } from "../navigation/RootStack";
@@ -11,6 +12,13 @@ const WelcomeScreen = ({ navigation }: WelcomeScreenProps) => {
       <Image
         source={require("../../assets/images/welcome.webp")}
         className="w-full h-full absolute"
+      />
+
+      <LinearGradient
+        colors={["transparent", "rgba(0,0,0,0.75)"]}
+        className="absolute bottom-0 left-0 right-0 top-0"
+        start={{ x: 0.5, y: 0.2 }}
+        end={{ x: 0.5, y: 1 }}
       />
 
       <View className="p-5 pb-10">
