4fbd25b09fa1e38085655c3f8ccaf95a0a8952cc
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-04 00:05:21 +0100

    feat(home): add headline text

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const HomeScreen = () => {
   return (
     <SafeAreaView className="flex flex-1 bg-white">
-      <ScrollView
-        className="mx-5"
-        showsVerticalScrollIndicator={false}
-      ></ScrollView>
+      <ScrollView className="mx-5" showsVerticalScrollIndicator={false}>
+        {/* Home Header */}
+        <View className="flex-row justify-between items-center my-4">
+          <Text className="font-bold text-neutral-700 text-3xl">
+            Explore Top Places
+          </Text>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
