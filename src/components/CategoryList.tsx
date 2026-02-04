f011a2b82442378d93b895be5c2e7d3bb0ce69be
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-04 02:57:46 +0100

    feat(home): add category list component

---
+++ src/components/CategoryList.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { Category } from "../types/category";
+import CategoryItem from "./CategoryItem";
+
+type CategoryListProps = {
+  categories: Category[];
+};
+
+const CategoryList = ({ categories }: CategoryListProps) => {
+  return (
+    <View className="w-full h-25 my-4">
+      <View className="flex-row justify-between items-end my-4 ">
+        <Text className="font-semibold text-neutral-600 text-2xl">
+          Categories
+        </Text>
+
+        <TouchableOpacity>
+          <Text className="font-bold text-red-300 text-lg underline">
+            See all
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={categories}
+        renderItem={({ item }) => <CategoryItem {...item} />}
+        keyExtractor={(item) => item.id.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        removeClippedSubviews
+        initialNumToRender={4}
+      />
+    </View>
+  );
+};
+
+export default CategoryList;
