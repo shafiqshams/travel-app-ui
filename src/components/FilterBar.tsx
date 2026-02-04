9ab29e71ba148c533eaed79b4978750c67b0f9b6
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-04 23:51:39 +0100

    feat: add filter bar view styles

--- src/components/FilterBar.tsx
+++ src/components/FilterBar.tsx
@@ -4,9 +4,9 @@ import { Text, TouchableOpacity, View } from "react-native";
 const FilterBar = () => {
   const filters = ["All", "Popular", "Recommended", "More"];
   const [selectedFilter, setSelectedFilter] = React.useState<string>("Popular");
-  console.log("selectedFilter", selectedFilter);
+
   return (
-    <View>
+    <View className="flex flex-row justify-around items-center my-8 p-1 bg-neutral-100 rounded-full">
       {filters.map((filter) => {
         return (
           <TouchableOpacity
