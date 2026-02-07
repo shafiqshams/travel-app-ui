86d925f6cd3a58b942041495bf6ede6ab70a7b5c
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-07 18:31:44 +0100

    feat(utils): add selectedFilter condition and a getter definition

--- src/utils/destinations.ts
+++ src/utils/destinations.ts
@@ -3,8 +3,13 @@ import { Destination } from "../types/destination";
 export const filterDestinations = (
   destinations: Destination[],
   searchText: string,
+  selectedFilter: string = "All",
 ): Destination[] => {
   let results = destinations;
+
+  if (selectedFilter !== "All") {
+    results = getFilteredDestinations(selectedFilter, destinations);
+  }
 
   const query = searchText?.trim().toLowerCase();
 
